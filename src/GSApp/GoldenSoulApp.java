76ff654ca33aa20e4fa82d26d09b35959a818b57
raisachilba <maya.raisa.anta@gmail.com>
2025-10-02 09:21:45 +0200

    Continuar programa

--- src/GSApp/GoldenSoulApp.java
+++ src/GSApp/GoldenSoulApp.java
@@ -5,6 +5,7 @@ import processing.core.PApplet;
 public class GoldenSoulApp extends PApplet {
 
     Colors c;
+    Fonts fontsApp;
 
     public static void main(String[] args) {
         PApplet.main("GSApp.GoldenSoulApp");
@@ -16,10 +17,23 @@ public class GoldenSoulApp extends PApplet {
 
     public void setup(){
         c = new Colors(this);
+        fontsApp = new Fonts(this);
     }
 
     public void draw(){
         background(255);
         c.display(this, 100, 100, width - 200);
+
+        fill(50);
+        textFont(fontsApp.getFirstFont());
+        text("Títol de l'app", 50, 410);
+
+        fill(50, 0, 0);
+        textFont(fontsApp.getSecondFont());
+        text("Subtítol de l'app", 50,   490);
+
+        fill(0, 50, 0);
+        textFont(fontsApp.getFirstFont());
+        text("Paràgraf de l'app", 50, 580);
     }
 }
